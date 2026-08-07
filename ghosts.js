9f9d59a650aa0db1236628c799fcf76537d8b381
JacobPHAS <welsteadjacob@gmail.com>
2026-08-07 12:18:11 +0100

    Add files via upload

---
+++ ghosts.js
@@ -0,0 +1,122 @@
+const ghosts = [
+    {
+        name: "Spirit",
+        evidence: ["EMF Level 5", "Spirit Box", "Ghost Writing"]
+    },
+
+    {
+        name: "Wraith",
+        evidence: ["EMF Level 5", "Spirit Box", "DOTS"]
+    },
+
+    {
+        name: "Phantom",
+        evidence: ["Spirit Box", "UV", "DOTS"]
+    },
+
+    {
+        name: "Poltergeist",
+        evidence: ["Spirit Box", "UV", "Ghost Writing"]
+    },
+
+    {
+        name: "Banshee",
+        evidence: ["UV", "DOTS", "Ghost Orb"]
+    },
+
+    {
+        name: "Jinn",
+        evidence: ["EMF Level 5", "UV", "Freezing Temperatures"]
+    },
+
+    {
+        name: "Mare",
+        evidence: ["Spirit Box", "Ghost Orb", "Ghost Writing"]
+    },
+
+    {
+        name: "Revenant",
+        evidence: ["Ghost Orb", "Ghost Writing", "Freezing Temperatures"]
+    },
+
+    {
+        name: "Shade",
+        evidence: ["EMF Level 5", "Ghost Writing", "Freezing Temperatures"]
+    },
+
+    {
+        name: "Demon",
+        evidence: ["UV", "Ghost Writing", "Freezing Temperatures"]
+    },
+
+    {
+        name: "Yurei",
+        evidence: ["Ghost Orb", "Freezing Temperatures", "DOTS"]
+    },
+
+    {
+        name: "Oni",
+        evidence: ["EMF Level 5", "Freezing Temperatures", "DOTS"]
+    },
+
+    {
+        name: "Yokai",
+        evidence: ["Spirit Box", "Ghost Orb", "DOTS"]
+    },
+
+    {
+        name: "Hantu",
+        evidence: ["UV", "Ghost Orb", "Freezing Temperatures"]
+    },
+
+    {
+        name: "Goryo",
+        evidence: ["EMF Level 5", "UV", "DOTS"]
+    },
+
+    {
+        name: "Myling",
+        evidence: ["EMF Level 5", "UV", "Ghost Writing"]
+    },
+
+    {
+        name: "Onryo",
+        evidence: ["Spirit Box", "Ghost Orb", "Freezing Temperatures"]
+    },
+
+    {
+        name: "The Twins",
+        evidence: ["EMF Level 5", "Spirit Box", "Freezing Temperatures"]
+    },
+
+    {
+        name: "Raiju",
+        evidence: ["EMF Level 5", "Ghost Orb", "DOTS"]
+    },
+
+    {
+        name: "Obake",
+        evidence: ["EMF Level 5", "UV", "Ghost Orb"]
+    },
+
+    {
+        name: "The Mimic",
+        evidence: ["Spirit Box", "UV", "Freezing Temperatures"]
+    },
+
+    {
+        name: "Moroi",
+        evidence: ["Spirit Box", "Ghost Writing", "Freezing Temperatures"]
+    },
+
+    {
+        name: "Deogen",
+        evidence: ["Spirit Box", "Ghost Writing", "DOTS"]
+    },
+
+    {
+        name: "Thaye",
+        evidence: ["Ghost Orb", "Ghost Writing", "DOTS"]
+    }
+
+];
